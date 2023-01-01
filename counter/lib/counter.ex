0d8eb3dcfe0ac3b130749fa0932ca595e719490e
Ossi Pettersson <ossi@moosh.dev>
2023-01-01 19:05:41 +0200

    Add comments

--- counter/lib/counter.ex
+++ counter/lib/counter.ex
@@ -1,21 +1,26 @@
 defmodule Counter do
   @moduledoc """
   Interact with processes.
+
+  This is essentially our API Layer
   """
 
   def start(initial_count) do
+    # Spawn spawns the given function and returns its PID
     spawn(fn -> Counter.Server.run(initial_count) end)
   end
 
   # Sends a :tick message to the server,
   # which increments count by 1
   def tick(pid) do
+    # Send sends a message to the given `dest` and returns the message.
     send(pid, {:tick, self()})
   end
 
   # Sends a :state message to the server, the server will in turn
   # send a response with the updated count: {:count, count}
   def state(pid) do
+    # Send sends a message to the given `dest` and returns the message.
     send(pid, {:state, self()})
 
     receive do
