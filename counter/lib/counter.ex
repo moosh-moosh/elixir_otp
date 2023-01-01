d4e071c90648a0bd38ec25f66c518334a49ba404
Ossi Pettersson <ossi@moosh.dev>
2023-01-01 18:54:16 +0200

    Update Counter
    Interact with our processes by sending messages to the server and
    receiving responses when expected.

--- counter/lib/counter.ex
+++ counter/lib/counter.ex
@@ -1,18 +1,25 @@
 defmodule Counter do
   @moduledoc """
-  Documentation for `Counter`.
+  Interact with processes.
   """
 
-  @doc """
-  Hello world.
+  def start(initial_count) do
+    spawn(fn -> Counter.Server.run(initial_count) end)
+  end
 
-  ## Examples
+  # Sends a :tick message to the server,
+  # which increments count by 1
+  def tick(pid) do
+    send(pid, {:tick, self()})
+  end
 
-      iex> Counter.hello()
-      :world
+  # Sends a :state message to the server, the server will in turn
+  # send a response with the updated count: {:count, count}
+  def state(pid) do
+    send(pid, {:state, self()})
 
-  """
-  def hello do
-    :world
+    receive do
+      {:count, value} -> value
+    end
   end
 end
