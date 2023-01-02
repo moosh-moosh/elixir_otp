157f83120cc0df27e8ae0df5d450a26b4bbcd1f4
Ossi Pettersson <ossi@moosh.dev>
2023-01-02 21:14:50 +0200

    Add initial Mastery.Core.Response
    Data layer for response

---
+++ mastery/lib/mastery/core/response.ex
@@ -0,0 +1,19 @@
+defmodule Mastery.Core.Response do
+  @moduledoc """
+  quiz_title:
+    Title field from the quiz.
+  template_name:
+    Name field identifying the template.
+  to
+    The question being answered, as in "this is a response /to/ the asked question".
+  email
+    The email address of the user answering the question.
+  answer
+    The answer provided by the user.
+  correct
+    Whether the given answer was correct.
+  timestamp
+    The time the answer was provided
+  """
+  defstruct ~w[quiz_title template_name to email answer correct timestamp]a
+end
