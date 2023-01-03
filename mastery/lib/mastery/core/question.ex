d5a9e80184c8b04c360f9debcc7c7ca41592957d
ossi <ossi@moosh.dev>
2023-01-03 21:09:21 +0200

    Add core functions to: Response, Question, Template
    Building the functional core.

--- mastery/lib/mastery/core/question.ex
+++ mastery/lib/mastery/core/question.ex
@@ -1,3 +1,39 @@
 defmodule Mastery.Core.Question do
+  alias Mastery.Core.Template
+
   defstruct ~w[asked substitutions template]a
+
+  def new(%Template{} = template) do
+    template.generators
+    |> Enum.map(&build_substitution/1)
+    |> evaluate(template)
+  end
+
+  def build_substitution({name, choices_or_generator}) do
+    {name, choose(choices_or_generator)}
+  end
+
+  # if choose called with a list of choices, pick one at random
+  defp choose(choices) when is_list(choices) do
+    Enum.random(choices)
+  end
+
+  # if choose called with a function that generates a substitution, call it
+  defp choose(generator) when is_function(generator) do
+    generator.()
+  end
+
+  defp compile(template, substitutions) do
+    template.compiled
+    |> Code.eval_quoted(assigns: substitutions)
+    |> elem(0)
+  end
+
+  defp evaluate(substitutions, template) do
+    %__MODULE__{
+      asked: compile(template, substitutions),
+      substitutions: substitutions,
+      template: template
+    }
+  end
 end
--- mastery/lib/mastery/core/template.ex
+++ mastery/lib/mastery/core/template.ex
@@ -2,4 +2,13 @@ defmodule Mastery.Core.Template do
   # sigil ~w creates a list of words, the a modifier at the end will create
   # a list of atoms instead of strings.
   defstruct ~w[name category instructions raw compiled generators checker]a
+
+  def new(fields) do
+    raw = Keyword.fetch!(fields, :raw)
+    struct!{
+      __MODULE__,
+      # EEx is a module used to compile idiomatic Elixir templates, called EEx templates.
+      Keyword.put(fields, :compiled, EEx.compile_string(raw))
+    }
+  end
 end
