ef6254e01951a4d97b60bb150b9e69c5c950d296
ossi <ossi@moosh.dev>
2023-01-04 21:23:03 +0200

    Add FIXME note for Mastery.Core.Template.new/1
    Mastery.Core.Template constructor currently not working.
    Should look into it
    
    ---
    
    ** (FunctionClauseError) no function clause matching in Kernel.struct!/2
    
    Attempted function clauses (showing 2 out of 2):
    
        def struct!(struct, fields) when is_atom(struct)
        def struct!(struct, fields) when is_map(struct)

--- mastery/lib/mastery/core/template.ex
+++ mastery/lib/mastery/core/template.ex
@@ -3,6 +3,8 @@ defmodule Mastery.Core.Template do
   # a list of atoms instead of strings.
   defstruct ~w[name category instructions raw compiled generators checker]a
 
+  # FIXME: This is not working currently
+  # ** (FunctionClauseError) no function clause matching in Kernel.struct!/2
   def new(fields) do
     raw = Keyword.fetch!(fields, :raw)
     struct!{
